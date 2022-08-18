edc9c492b772badb92ff167bfd0ba343d9e39100
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2022-08-18 08:19:17 -0300

    Created Hangman Game and Types

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,8 +1,21 @@
 defmodule Hangman do
 
-  def new_game do
-  end
+  alias Hangman.Impl.Game
 
+  @opaque game :: Game.t
+
+  @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
+  @type tally :: %{
+    game_state: state,
+    letters:    list(String.t),
+    turns_left: integer,
+    used:       list(String.t),
+  }
+
+  @spec new_game() :: game
+  defdelegate new_game, to: Game
+
+  @spec make_move(game, String.t) :: { game, tally }
   def make_move(game, guess) do
   end
 
---
+++ hangman/lib/impl/game.ex
@@ -0,0 +1,23 @@
+defmodule Hangman.Impl.Game do
+
+  @type t :: %__MODULE__{
+    game_state: Hangman.state,
+    letters:    list(String.t),
+    turns_left: integer,
+    used:       MapSet.t(String.t),
+  }
+
+  defstruct(
+    game_state: :initializing,
+    letters:    [],
+    turns_left: 7,
+    used:       MapSet.new(),
+  )
+
+  def new_game do
+    %__MODULE__{
+      letters: Dictionary.random_word |> String.codepoints 
+    }
+  end
+
+end
