a8bd0e1eab79b626ef68e6d2f8641f8ef16e927d
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2022-08-24 20:01:57 -0300

    Added supervisor to restart the server

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -2,15 +2,20 @@ defmodule Dictionary.Runtime.Server do
 
   @type t :: pid()
 
+  use Agent
+
   alias Dictionary.Impl.Words
 
   @me __MODULE__
 
-  def start_link do
+  def start_link(_) do
     Agent.start_link(&Words.word_list/0, name: @me)
   end
 
   def random_word do
+    if :rand.uniform < 0.33 do
+      Agent.get(@me, fn _ -> exit(:boom) end)
+    end
     Agent.get(@me, &Words.random_word/1)
   end
 
