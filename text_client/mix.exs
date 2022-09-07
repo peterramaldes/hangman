2736f60f2835c9c6dd5db43903600bebe454bbac
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2022-09-07 07:58:38 -0300

    Do not started hangman everytime we start text_client

--- text_client/mix.exs
+++ text_client/mix.exs
@@ -14,14 +14,15 @@ defmodule TextClient.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      included_applications: [ :hangman ],
+      extra_applications:    [ :logger  ],
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      { :hangman, path: "../hangman" },
+      hangman: [ path: "../hangman" ]
     ]
   end
 end
