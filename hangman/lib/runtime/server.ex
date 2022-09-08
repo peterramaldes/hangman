c7278cfdb91883865f85f815afb98cd4733e4435
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2022-09-07 21:26:44 -0300

    Added watchdog to kill the process after 1 hour

--- hangman/lib/runtime/server.ex
+++ hangman/lib/runtime/server.ex
@@ -1,29 +1,34 @@
 defmodule Hangman.Runtime.Server do
   @type t :: pid()
+  @idle_timeout 1 * 60 * 60 * 1000 # 1 hour
 
+  alias Hangman.Runtime.Watchdog
   alias Hangman.Impl.Game
 
   use GenServer
 
   # Client Process
-  
+
   def start_link(_) do
     GenServer.start_link(__MODULE__, nil)
   end
 
 
   # Server Process
-  
+
   def init(_) do
-    {:ok, Game.new_game()}
+    watcher = Watchdog.start(@idle_timeout)
+    {:ok, {Game.new_game(), watcher}}
   end
 
-  def handle_call({:make_move, guess}, _from, game) do
+  def handle_call({:make_move, guess}, _from, {game, watcher}) do
+    Watchdog.im_alive(watcher)
     {updated_game, tally} = Game.make_move(game, guess)
-    {:reply, tally, updated_game} 
+    {:reply, tally, {updated_game, watcher}} 
   end
 
-  def handle_call({:tally}, _from, game) do
-    {:reply, Game.tally(game), game}
+  def handle_call({:tally}, _from, {game, watcher}) do
+    Watchdog.im_alive(watcher)
+    {:reply, Game.tally(game), {game, watcher}}
   end
 end 
